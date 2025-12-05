434892f6f38e347785dbbef36964a5ccae7a3b2a
lucasalm7 <joxsal01@easv365.dk>
2025-12-05 19:26:25 +0100

    [fix] slideshow about page

--- js/main.js
+++ js/main.js
@@ -1,35 +1,37 @@
 let slideIndex = 1;
+if (document.getElementsByClassName("slideshow").length > 0) {
 slideshow(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  slideshow(slideIndex += n);
 }
 
-// Thumbnail image controls
+function plusSlides(n) {
+slideshow(slideIndex += n);
+}
+
 function currentSlide(n) {
-  slideshow(slideIndex = n);
+slideshow(slideIndex = n);
 }
 
 function slideshow(n) {
-  let i;
-  let slides = document.getElementsByClassName("slideshow");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+let i;
+let slides = document.getElementsByClassName("slideshow");
+let dots = document.getElementsByClassName("dot");
+if (slides.length === 0) return; // Safety check
+
+if (n > slides.length) {slideIndex = 1}
+if (n < 1) {slideIndex = slides.length}
+for (i = 0; i < slides.length; i++) {
+slides[i].style.display = "none";
+}
+for (i = 0; i < dots.length; i++) {
+dots[i].className = dots[i].className.replace(" active", "");
+}
+slides[slideIndex-1].style.display = "block";
+dots[slideIndex-1].className += " active";
 }
 
 let flkty = new Flickity('.main-gallery', {
-  // option
-  cellAlign: 'left',
-  contain: true,
-  freeScroll: true
+cellAlign: 'left',
+contain: true,
+freeScroll: true,
+wrapAround: true
 })
